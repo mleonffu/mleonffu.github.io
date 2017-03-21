26a0508715e86befd525ef2a54d46ae0b53e7490
LeonffuMichael <leonffum786@carlsbadusd.net>
2017-03-21 03:47:48 +0000

    This is my Message

--- gitAutoCommit.sh
+++ gitAutoCommit.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+function centerIt {
+    lines=$(tput lines);cols=$(tput cols);tput cup $((lines/2)) $(((cols-${#1})/2))
+}
+
+function centerPrintIt {
+    centerIt "$1"
+    printf "$1"
+}
+
 clear
-printf 'Auto commiting to github script; autocommits pwd\n\n'
+centerPrintIt '_____Auto commiting to github script; autocommits pwd_____\n\n'
+sleep 2; tput clear
+
+printf 'Fetching and Pulling... \nFetch:\n';git fetch
+printf '\nPulling:\n';git pull
 
 printf '%b\n' 'GitHub Auto Commit from pwd:' '\033[95m' $(pwd) '\033[0m'
 git add .
@@ -9,15 +22,20 @@ git add .
 printf 'Message to commit with: \n'
 read commitMessage
 
-printf '%b' '\nIs this your message:\n' '\033[95m' "$commitMessage" '\n'
+clear
+printf "Commit with this message?"
+printf "\n\033[95m $commitMessage \033[0m\n"
+
 read -n1 -p '(y/n)' messageConfirmation
-printf '\033[0m\n'
 
 if ( [[ "$messageConfirmation" = 'y' ]] );then
     git commit -m "$commitMessage"
     git push
+    printf 'Success!?'
+else    
+    printf 'Conformation Failed, Exiting \n'   
 fi
-printf 'Conformation Failed, Exiting \n'
 
 printf '\033[93m Have a Nice Day! \033[0m \n'
 
+exit 1
