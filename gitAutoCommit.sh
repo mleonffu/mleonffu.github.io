1d9b53e67af6e8cdfa1294e4ba5f8e76ae8f20d8
LeonffuMichael <leonffum786@carlsbadusd.net>
2017-03-21 02:42:25 +0000

    Auto Commit Test

--- gitAutoCommit.sh
+++ gitAutoCommit.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+clear
+printf 'Auto commiting to github script; autocommits pwd\n\n'
+
+printf '%b\n' 'GitHub Auto Commit from pwd:' '\033[95m' $(pwd) '\033[0m'
 git add .
-git commit -m Auto Commit
-git push
+
+printf 'Message to commit with: \n'
+read commitMessage
+
+printf '%b' '\nIs this your message:\n' '\033[95m' "$commitMessage" '\n'
+read -n1 -p '(y/n)' messageConfirmation
+printf '\n'
+
+if ( [[ "$messageConfirmation" = 'y' ]] );then
+    git commit -m "$commitMessage"
+    git push
+fi
+printf 'Conformation Failed, Exiting \n'
+
+printf '\033[93m Have a Nice Day! \033[0m \n'
+
